fc24614187fe2a3e723a95cf96e90368c17e5dab
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2024-04-22 18:14:30 +0600

    New package added and contact form updated

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -3,20 +3,59 @@ import React from "react";
 import { Label } from "@/components/common/Label";
 import { Input } from "@/components/common/Input";
 import { cn } from "@/utils/cn";
-// import {
-//   IconBrandGithub,
-//   IconBrandGoogle,
-//   IconBrandOnlyfans,
-// } from "@tabler/icons-react";
+import { BsWhatsapp } from "react-icons/bs";
+import { SiLinkedin } from "react-icons/si";
+import { MdLocalPhone } from "react-icons/md";
+import { SiGmail } from "react-icons/si";
+import { GrSkype } from "react-icons/gr";
 import BottomGradient from "./ui/BottomGradient";
 import { Textarea } from "./common/Textarea";
+
+type ContactButtonType = {
+  label: string,
+  icon: any,
+  extra: string,
+  link: string
+
+}
+
+const contactButtons: ContactButtonType[] = [
+  {
+    label: "Phone",
+    icon: <MdLocalPhone className="h-4 w-4" />,
+    extra: "+88 01856-878150",
+    link: "tel:+8801856878150"
+  },
+  {
+    label: "Gmail",
+    icon: <SiGmail className="h-4 w-4" />,
+    extra: "mahadih.dev@gmail.com",
+    link: "mailto:mahadih.dev@gmail.com"
+  },
+  {
+    label: "WhatsApp",
+    icon: <BsWhatsapp className="h-4 w-4 " />,
+    extra: "+8801856878150",
+    link: "https://wa.me/+8801856878150"
+  },
+  {
+    label: "LinkedIn",
+    icon: <SiLinkedin className="h-4 w-4" />,
+    extra: "linkedin.com/in/mahadih",
+    link: "https://www.linkedin.com/in/mahadih"
+  },
+  {
+    label: "Skype",
+    icon: <GrSkype className="h-4 w-4" />,
+    extra: "join.skype.com/invite/wbLw5G0YcO5G",
+    link: "https://join.skype.com/invite/wbLw5G0YcO5G"
+  },
+]
 
 export function ContactForm() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted", e);
-
-
   };
   return (
     <div className="">
@@ -52,34 +91,28 @@ export function ContactForm() {
 
         <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
-        <div className="flex flex-col space-y-4">
-          <button
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-dark rounded-md h-10 font-medium bg-gray-50 bg-zinc-900 shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-          >
-            {/* <IconBrandGithub className="h-4 w-4 text-neutral-300" /> */}
-            <span className="text-neutral-300 text-sm">
-              WhatsApp
-            </span>
-            <BottomGradient />
-          </button>
-          <button
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-dark rounded-md h-10 font-medium bg-zinc-900 shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-          >
-            {/* <IconBrandGoogle className="h-4 w-4 text-neutral-300" /> */}
-            <span className="text-neutral-300 text-sm">
-              Linked In
-            </span>
-            <BottomGradient />
-          </button>
-          <button
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-dark rounded-md h-10 font-medium bg-zinc-900 shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-          >
-            {/* <IconBrandOnlyfans className="h-4 w-4 text-neutral-300" /> */}
-            <span className="text-neutral-300 text-sm">
-              Skype
-            </span>
-            <BottomGradient />
-          </button>
+        <div className="grid gap-4">
+          {
+            contactButtons.map((buttonItem: ContactButtonType, i) => {
+              return (
+                <a
+                  key={i}
+                  href={buttonItem?.link}
+                  target="_blank"
+                  className="relative group/btn flex-start px-4 space-x-2 w-full text-light rounded-md h-10 font-medium bg-gray-50 bg-zinc-900 shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                >
+                  <span className="flex-start space-x-2 min-w-36">
+                    {buttonItem?.icon}
+                    <span className="text-base">
+                      {buttonItem?.label}
+                    </span>
+                  </span>
+                  <span className="font-extralight text-gray text-sm">{buttonItem?.extra}</span>
+                  <BottomGradient />
+                </a>
+              )
+            })
+          }
         </div>
       </form>
     </div>
--- src/data/about.ts
+++ src/data/about.ts
@@ -3,7 +3,7 @@ export const aboutData = [
 
   "With over 3 years of experience, I've honed my skills in React JS and Next JS development while collaborating on diverse projects. From microservice architecture projects to e-commerce platforms, portfolios, ERP software, dashboards, and presentation management systems, I've gained valuable expertise in Tailwind CSS and Ant Design to create sleek and intuitive designs. Additionally, as a good team player, I thrive in collaborative environments, contributing effectively to project success while ensuring seamless communication and coordination among team members.",
 
-  "My skillset includes React JS, Next JS, Tailwind CSS, Ant Design, Maintine UI.I excel in Agile / Scrum environments, ensuring efficient project delivery.",
+  "My skillset includes React JS, Next JS, Tailwind CSS, Ant Design, Maintine UI. I excel in Agile / Scrum environments, ensuring efficient project delivery.",
 
   "Experienced in developing user-friendly Webb Application, Dashboards, Authentication systems, and dynamic web features.",
 
